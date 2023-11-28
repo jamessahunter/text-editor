05dc986cb4d17dbd417a3d07f5e3b3ebe2bce7ea
James Hunter <jamessahunter@gmail.com>
2023-11-28 15:40:23 -0800

    add install logic and asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,19 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  // Name of the cache storage.
+  cacheName: 'asset-cache',
+  plugins: [
+    // CacheableResponsePlugin is used to configure which response statuses are considered cacheable. 
+    // In this case, responses with status codes 0 (offline) and 200 (OK) are considered cacheable.
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}));
 
 // TODO: Implement asset caching
 registerRoute();
